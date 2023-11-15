acf9d21c1f5696a799056fdd3928d0fde922f44d
Lodenius <yhfe22.lodeph@folkuniversitetet.nu>
2023-11-15 11:22:31 +0100

    food-card-commit-1

--- Frontend/src/components/FoodCard.jsx
+++ Frontend/src/components/FoodCard.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-function FoodCard() {
+function FoodCard({price, pieces}) {
     return ( 
-        <article>
-            
+        <article className="FoodCard">
+            <section>
+                <img src="./public/assets/img/mat.svg" alt=""/>
+                <section className="info">
+                    <p>{pieces} 12pc mix</p>
+                    <p>{price} 130 sek</p>
+                </section>
+            </section>
         </article>
      );
 }
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroFood from '../components/HeroFood';
+import FoodCard from '../components/FoodCard';
 
 function HomePage() {
   return (
     <main className='HomePage'>
         <HeroFood />
+        <FoodCard />
     </main>
     
     )

e869e890429fbfbf62f9b473888853d5c57e0457
malekmahfoudh <malek.mahfoudh7@me.com>
2023-11-17 12:40:41 +0100

    fixed hero slides

--- Frontend/src/components/HeroFood.jsx
+++ Frontend/src/components/HeroFood.jsx
@@ -16,17 +16,20 @@ function HeroFood() {
         autoplay={{delay: 2500,
             disableOnInteraction: false,}}
         modules={[Autoplay]}
-
+        initialSlide={1}
         className="mySwiper"
       >
                 <SwiperSlide>
-                    <img src="./public/assets/img/image1.svg" alt="food" />
+                    <img src="./public/assets/img/sushi-hero-3.svg" alt="food" />
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src="./public/assets/img/image1.svg" alt="food" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="./public/assets/img/image1.svg" alt="food" />
+                    <img src="./public/assets/img/sushi-hero.svg" alt="food" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="./public/assets/img/sushi-hero-2.svg" alt="food" />
                 </SwiperSlide>
             </Swiper>
         </main>

2be153f93a5373cffcf4b5b5feeac9c85ab4ca45
Lodenius <yhfe22.lodeph@folkuniversitetet.nu>
2023-11-17 09:56:02 +0100

    profile-page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
+import MyProfile from './pages/MyProfile.jsx'
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-
+        <Route path="/profile" element={<MyProfile />} />
       </Routes>
     </BrowserRouter>
 

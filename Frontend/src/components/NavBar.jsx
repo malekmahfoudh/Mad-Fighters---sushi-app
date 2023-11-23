9400524f2bcb63bfb5dfaa93836e9c4f2da871c8
Lodenius <yhfe22.lodeph@folkuniversitetet.nu>
2023-11-23 08:33:08 +0100

    nav fix

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -1,12 +1,17 @@
-// import { useState } from "react";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function NavBar({ account, menu, cart }) {
      return (
         <footer className="nav-container">
           <section className="bubble-container">
                 <div style={account}></div>
+            <NavLink to="/">
                 <div style={menu}></div>
+            </NavLink>
+            <NavLink to="cart">
                 <div style={cart}></div>
+            </NavLink>
             </section>
             <section className="icon-container">
                 <section>

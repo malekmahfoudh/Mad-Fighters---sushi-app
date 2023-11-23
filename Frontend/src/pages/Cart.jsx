48c7ebf4fef268d459a25a82ebae18a05f537a52
Lodenius <yhfe22.lodeph@folkuniversitetet.nu>
2023-11-23 08:26:13 +0100

    nav-fix

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
 import EmptyCart from '../components/EmptyCart';
+import NavBar from '../components/NavBar';
 
 function Cart() {
     return ( 
         <section className='CartContainer'>
             <Header />
             <EmptyCart />
+            <NavBar 
+          cart={{opacity: '1'}}
+        />
         </section>
      );
 }

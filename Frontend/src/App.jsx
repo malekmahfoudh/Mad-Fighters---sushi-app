983a47dbedb4c61ab6de62f074003abd5f62cef0
malekmahfoudh <malek.mahfoudh7@me.com>
2023-12-12 15:44:13 +0100

    draggable button

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,39 +1,32 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addProducts } from "./redux/slices/productsSlice.js";
-
 import HomePage from "./pages/HomePage.jsx";
 import MyProfile from "./pages/MyProfile.jsx";
 import Cart from "./pages/Cart.jsx";
 import About from "./pages/About.jsx";
 import Login from "./pages/Login.jsx";
 import Menu from "./pages/Menu.jsx";
-import { addProductToCart } from "./redux/slices/cart.js";
-import NewOrders from './pages/NewOrders.jsx'
-import PreparingOrders from './pages/PreparingOrders.jsx'
-import ReadyOrders from './pages/ReadyOrders.jsx'
 
+import NewOrders from "./pages/NewOrders.jsx";
+import PreparingOrders from "./pages/PreparingOrders.jsx";
+import ReadyOrders from "./pages/ReadyOrders.jsx";
 
 function App() {
   const dispatch = useDispatch();
-  //fetch from the remote server and add the data to the redux store
-  useEffect(()=> {
-    fetch('https://sushi-vibes.onrender.com/api/menu')
-    .then(res => res.json())
-    .then(data => {
-      dispatch(addProducts(data.menu));
-    });
-
-  },[]);
+  useEffect(() => {
+    fetch("https://sushi-vibes.onrender.com/api/menu")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch(addProducts(data.menu));
+      });
+  }, []);
 
   return (
-    
-      <BrowserRouter>
-    
-        <Routes>
+    <BrowserRouter>
+      <Routes>
         <Route path="/home" element={<HomePage />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/profile" element={<MyProfile />} />
@@ -44,8 +37,7 @@ function App() {
         <Route path="/preparing-orders" element={<PreparingOrders />} />
         <Route path="/ready-orders" element={<ReadyOrders />} />
       </Routes>
-        
-      </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -4,38 +4,55 @@ import SearchBar from "../components/SearchBar";
 import FeaturedFoods from "../components/FeaturedFoods";
 import NavBar from "../components/NavBar";
 import { useState } from "react";
+import { useRef } from "react";
+import { motion } from "framer-motion";
 
 import { ToastContainer } from "react-toastify";
 
 function HomePage() {
   const [selectedCategory, setSelectedCategory] = useState("");
 
+  const constraintsRef = useRef(null);
+
   return (
     <main className="HomePage">
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-      <Header />
-      <HeroFood />
-      <SearchBar
-        setSelectedCategory={setSelectedCategory}
-        selectedCategory={selectedCategory}
-      />
-      <article>
-        <h3>
-          {selectedCategory ? selectedCategory.toUpperCase() : "TOP CHOICES"}
-        </h3>
-      </article>
-      <FeaturedFoods selectedCategory={selectedCategory} />
+      <motion.div 
+        className="drag-area" 
+        ref={constraintsRef}
+        >
+        <motion.button
+          className="draggable_button"
+          drag
+          dragConstraints={constraintsRef}
+          style={{ position: "absolute", zIndex: 1000 }}
+        >
+        </motion.button>
+
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+        <Header />
+        <HeroFood />
+        <SearchBar
+          setSelectedCategory={setSelectedCategory}
+          selectedCategory={selectedCategory}
+        />
+        <article>
+          <h3>
+            {selectedCategory ? selectedCategory.toUpperCase() : "TOP CHOICES"}
+          </h3>
+        </article>
+        <FeaturedFoods selectedCategory={selectedCategory} />
+      </motion.div>
       <NavBar />
     </main>
   );

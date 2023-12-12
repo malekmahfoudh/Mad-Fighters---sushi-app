71f19e733d25a32eb76e2963fe986097774c8d82
lindakahju <116061883+lindakahju@users.noreply.github.com>
2023-12-12 12:33:11 +0100

    profile changes

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,27 +1,44 @@
 import React from "react";
-import '../styles/Header.scss'
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate, useLocation } from 'react-router-dom';
+import '../styles/Header.scss';
 
 function Header() {
     const navigation = useNavigate();
+    const location = useLocation();
+
     const handleClick = () => {
         navigation('/');
     }
-    return ( 
+
+    const getUserInfo = () => {
+        if (location.pathname === '/home' || location.pathname === '/cart' || location.pathname === '/menu') {
+            return <h3>Hello, customer!</h3>;
+        } else {
+            return <h3>Hello, worker!</h3>;
+        }
+    }
+
+    const getUserImage = () => {
+        if (location.pathname === '/home' || location.pathname === '/cart' || location.pathname === '/menu') {
+            return <img src="./public/assets/icons/customer.jpg" alt="profile image" />
+        } else  {
+            return <img src="./public/assets/icons/worker.png" alt="profile image" />
+        }
+    }
+
+    return (
         <header className="HeaderContainer">
             <img className="Logo" src="./public/assets/img/header-logo.svg" alt="sushi vibes logo" onClick={handleClick} />
-
             <section className="Profile">
                 <section className="ProfileInfo">
-                    <h3>Hello, John</h3>
-                    <p>Branteviksgatan 5B</p>
+                    {getUserInfo()}
                 </section>
                 <NavLink to='/profile'>
-                    <img src="./public/assets/img/profile-image.svg" alt="profile image" />
+                    {getUserImage()}
                 </NavLink>
             </section>
         </header>
-     );
+    );
 }
 
 export default Header;
